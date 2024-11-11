e3be61737df49b53725ad3ac63ccc870227d42d1
nhat <nhatguillon@gmail.com>
2024-11-11 16:54:21 +1100

    Create a 400x400 grid with divs

---
+++ script.js
@@ -0,0 +1,72 @@
+const container = document.querySelector("#container");
+
+const square1 = document.createElement("div");
+square1.classList.add("line1");
+
+const square2 = document.createElement("div");
+square2.classList.add("line1");
+
+const square3 = document.createElement("div");
+square3.classList.add("line1");
+
+const square4 = document.createElement("div");
+square4.classList.add("line1");
+
+
+const square5 = document.createElement("div");
+square5.classList.add("line2");
+
+const square6 = document.createElement("div");
+square6.classList.add("line2");
+
+const square7 = document.createElement("div");
+square7.classList.add("line2");
+const square8 = document.createElement("div");
+square8.classList.add("line2");
+
+
+const square9 = document.createElement("div");
+square9.classList.add("line3");
+
+const square10 = document.createElement("div");
+square10.classList.add("line3");
+
+const square11 = document.createElement("div");
+square11.classList.add("line3");
+
+const square12 = document.createElement("div");
+square12.classList.add("line3");
+
+
+const square13 = document.createElement("div");
+square13.classList.add("line4");
+
+const square14 = document.createElement("div");
+square14.classList.add("line4");
+
+const square15 = document.createElement("div");
+square15.classList.add("line4");
+
+const square16 = document.createElement("div");
+square16.classList.add("line4");
+
+
+container.appendChild(square1);
+container.appendChild(square2);
+container.appendChild(square3);
+container.appendChild(square4);
+
+container.appendChild(square5);
+container.appendChild(square6);
+container.appendChild(square7);
+container.appendChild(square8);
+
+container.appendChild(square9);
+container.appendChild(square10);
+container.appendChild(square11);
+container.appendChild(square12);
+
+container.appendChild(square13);
+container.appendChild(square14);
+container.appendChild(square15);
+container.appendChild(square16);
