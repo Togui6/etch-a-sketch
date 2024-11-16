ba11bfaac02322e2c70c6d2726874843dc819720
nhat <nhatguillon@gmail.com>
2024-11-16 22:10:15 +1100

    Add hover effect for all divs

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # etch-a-sketch
+
+
+
+
+
+
+Individual square light up : 
+
+const hover1 = document.querySelector(".square1");
+hover1.onmouseover = function() {
+    hover1.style.background = "blue";
+};
+hover1.onmouseout = function() {
+    hover1.style.background = "white";
+};
+
+const hover2 = document.querySelector(".square2");
+hover2.onmouseover = function() {
+    hover2.style.background = "yellow";
+};
+hover2.onmouseout = function() {
+    hover2.style.background = "white";
+};
+
+const hover3 = document.querySelector(".square3");
+hover3.onmouseover = function() {
+    hover3.style.background = "green";
+};
+hover3.onmouseout = function() {
+    hover3.style.background = "white";
+};
+
+const hover4 = document.querySelector(".square4");
+hover4.onmouseover = function() {
+    hover4.style.background = "red";
+};
+hover4.onmouseout = function() {
+    hover4.style.background = "white";
+};
--- script.js
+++ script.js
@@ -1,54 +1,55 @@
 const container = document.querySelector("#container");
 
 const square1 = document.createElement("div");
-square1.classList.add("line1");
+square1.classList.add("square");
+
 
 const square2 = document.createElement("div");
-square2.classList.add("line1");
+square2.classList.add("square");
 
 const square3 = document.createElement("div");
-square3.classList.add("line1");
+square3.classList.add("square");
 
 const square4 = document.createElement("div");
-square4.classList.add("line1");
+square4.classList.add("square");
 
 
 const square5 = document.createElement("div");
-square5.classList.add("line2");
+square5.classList.add("square");
 
 const square6 = document.createElement("div");
-square6.classList.add("line2");
+square6.classList.add("square");
 
 const square7 = document.createElement("div");
-square7.classList.add("line2");
+square7.classList.add("square");
 const square8 = document.createElement("div");
-square8.classList.add("line2");
+square8.classList.add("square");
 
 
 const square9 = document.createElement("div");
-square9.classList.add("line3");
+square9.classList.add("square");
 
 const square10 = document.createElement("div");
-square10.classList.add("line3");
+square10.classList.add("square");
 
 const square11 = document.createElement("div");
-square11.classList.add("line3");
+square11.classList.add("square");
 
 const square12 = document.createElement("div");
-square12.classList.add("line3");
+square12.classList.add("square");
 
 
 const square13 = document.createElement("div");
-square13.classList.add("line4");
+square13.classList.add("square");
 
 const square14 = document.createElement("div");
-square14.classList.add("line4");
+square14.classList.add("square");
 
 const square15 = document.createElement("div");
-square15.classList.add("line4");
+square15.classList.add("square");
 
 const square16 = document.createElement("div");
-square16.classList.add("line4");
+square16.classList.add("square");
 
 
 container.appendChild(square1);
@@ -70,3 +71,20 @@ container.appendChild(square13);
 container.appendChild(square14);
 container.appendChild(square15);
 container.appendChild(square16);
+
+
+
+
+
+
+
+const allDivs = document.querySelectorAll(".square");
+allDivs.forEach(carre => {
+    carre.addEventListener('mouseover', () => {
+carre.style.background = "red";
+});
+
+carre.addEventListener('mouseout', () => {
+carre.style.background = "white";
+});
+});
